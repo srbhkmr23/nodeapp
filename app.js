f5e203ea68948cee11d087a3982b0fd2ccc8a5ae
srbhkmr23 <srbhkmr23@gmail.com>
2017-03-01 12:52:11 +0530

    update app.js

--- app.js
+++ app.js
@@ -27,11 +27,11 @@ app.get('/', function (req, res) {
 res.end("Hello node server");
 })
 
-var server_port=process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address=process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+//var server_port=process.env.OPENSHIFT_NODEJS_PORT || 8080;
+//var server_ip_address=process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 
-var server = app.listen(server_port,server_ip_address,function () {
+var server = app.listen(8080,function () {
   var host = server.address().address;
   var port = server.address().port;
 
